3b97b1a423001db2e12d77c171f0eaf922fc8ba9
Alexander Evsyuchenya <alexander.evsyuchenya@adms-imac-2.hiqo-solutions.com>
2016-02-19 18:25:03 +0300

    alfa-version of skeleton

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "tgaimage.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	bool reversed = false;
@@ -10,23 +14,51 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 		std::swap(x1, y1);
 		reversed = true;
 	}
-	if (x0 > x1) std::swap(x0, x1);
+	if (x0>x1) {
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int derr = dy * 2;
+	int err = 0;
+	int y = y0;
 	for (int x = x0; x <= x1; ++x) {
-		int y = ((x - x0) * (y1 - y0)) / (float)(x1-x0) + y0;
 		if (reversed) {
 			image.set(y, x, color);
 		} else {
 			image.set(x, y, color);
 		}
+		err += derr;
+
+		if (err > dx) {
+			y += y1 > y0 ? 1 : -1;
+			err -= 2 * dx;
+		}
+
 	}
 }
 
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	line(13, 20, 80, 40, image, white);
-	line(20, 13, 40, 80, image, red);
-	line(80, 40, 13, 20, image, red);
+
+  model = new Model("Projects/Other/tinyrenderer/obj/african_head.obj");
+
+	TGAImage image(width, height, TGAImage::RGB);
+	for (int i=0; i<model->nfaces(); i++) {
+	    std::vector<int> face = model->face(i);
+	    for (int j=0; j<3; j++) {
+	        Vec3f v0 = model->vert(face[j]);
+	        Vec3f v1 = model->vert(face[(j+1)%3]);
+	        int x0 = (v0.x+1.)*width/2.;
+	        int y0 = (v0.y+1.)*height/2.;
+	        int x1 = (v1.x+1.)*width/2.;
+	        int y1 = (v1.y+1.)*height/2.;
+	        line(x0, y0, x1, y1, image, white);
+	    }
+	}
+
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("Projects/Other/tinyrenderer/test.tga");
+	image.write_tga_file("Projects/Other/tinyrenderer/output.tga");
+	delete model;
 	return 0;
 }
