e55e0fb07744c0d8d3e6c2190ec44b20999cbe24
Alexander Evsyuchenya <alexander.evsyuchenya@adms-imac-2.hiqo-solutions.com>
2016-03-01 18:09:53 +0300

    adding perspective

--- main.cpp
+++ main.cpp
@@ -11,6 +11,32 @@ const int width  = 800;
 const int height = 800;
 const int depth  = 255;
 Vec3f light_dir(0,0,-1);
+Vec3f camera(0,0,3);
+
+Vec3f m2v(Matrix m) {
+    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);
+}
+
+Matrix v2m(Vec3f v) {
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
+
+Matrix viewport(int x, int y, int w, int h) {
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;
+    m[1][3] = y+h/2.f;
+    m[2][3] = depth/2.f;
+
+    m[0][0] = w/2.f;
+    m[1][1] = h/2.f;
+    m[2][2] = depth/2.f;
+    return m;
+}
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	bool reversed = false;
